faa7a449f2b0f034710fa99cf27ae24e3ed3cd27
MasashiShimamoto <masashishimamoto@gmail.com>
2025-02-07 02:32:59 +0900

    a

--- tradingtools/Trend-Checker-Linear-Regression/js/multiLang.js
+++ tradingtools/Trend-Checker-Linear-Regression/js/multiLang.js
@@ -1,13 +1,15 @@
-import { translations } from 'https://onetickfor.win/tradingtools/Trend-Checker-Linear-Regression/js/translations.js';
+let ja =  "このツールは、線形回帰と三角法を活用し、トレンドや反転ポイントを瞬時に見極めるのに役立ちます。ダウ理論やグランビルの法則のような複雑な手法に頼る必要はありません。シンプルで分かりやすく、迷わず自信を持ってトレードできるように設計されています。";
+let en = "This tool uses linear regression and trigonometry to help you spot trends and reversal points in an instant. You won’t need to rely on complicated methods like Dow Theory or Granville’s Rules. It’s simple, clear, and designed to let you focus on confident trading without the guesswork.";
+
 
 document.addEventListener("DOMContentLoaded",function(){
     let params = new URLSearchParams(window.location.search);
     let resultText = "";
 
     if(params.get("lang")==="ja"){
-        resultText = translations.ja;
+        resultText = ja;
     }else{
-        resultText = translations.en;
+        resultText = en;
     }
 
     document.getElementById("sub-copy").textContent = resultText;
