68296903c3655437d438b7a0e0bef082cb8906c0
MasashiShimamoto <masashishimamoto@gmail.com>
2025-02-11 00:39:22 +0900

    a

--- tradingtools/Trend-Checker-Linear-Regression/js/redirect.js
+++ tradingtools/Trend-Checker-Linear-Regression/js/redirect.js
@@ -6,33 +6,29 @@ document.addEventListener("DOMContentLoaded", function() {
     let select = currentUrl.searchParams.get("select");
     let query = null;
 
-    /*if (userLang.includes("ja")) {
+    if (userLang.includes("ja")) {
         query = "ja";
     }else if (userLang.includes("ru")) {
         query = "ru";
-    }else if (userLang.includes("ja")) {
-        query = "ja";
-    }else if (userLang.includes("ja")) {
-        query = "ja";
-    }else if (userLang.includes("ja")) {
-        query = "ja";
-    }else if (userLang.includes("ja")) {
-        query = "ja";
-    }else if (userLang.includes("ja")) {
-        query = "ja";
-    }else if (userLang.includes("ja")) {
-        query = "ja";
-    }else if (userLang.includes("ja")) {
-        query = "ja";
-    }else if (userLang.includes("ja")) {
-        query = "ja";
-    }*/
-
-    if(!userLang.includes(lang)){
-        query=lang;
+    }else if (userLang.includes("zh")) {
+        query = "zh";
+    }else if (userLang.includes("es")) {
+        query = "es";
+    }else if (userLang.includes("pt")) {
+        query = "pt";
+    }else if (userLang.includes("de")) {
+        query = "de";
+    }else if (userLang.includes("ko")) {
+        query = "ko";
+    }else if (userLang.includes("fr")) {
+        query = "fr";
+    }else if (userLang.includes("it")) {
+        query = "it";
+    }else if (userLang.includes("tr")) {
+        query = "tr";
     }
 
-    if (!select && query) {
+    if (!select && query && query !== lang) {
         window.location.href = url + "?lang=" + query;
     }
 });
