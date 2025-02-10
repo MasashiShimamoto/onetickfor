55a0294b9f1bc1686fa587ab989ff603b1f3398d
MasashiShimamoto <masashishimamoto@gmail.com>
2025-02-10 23:05:56 +0900

    a

--- tradingtools/Trend-Checker-Linear-Regression/js/redirect.js
+++ tradingtools/Trend-Checker-Linear-Regression/js/redirect.js
@@ -10,6 +10,58 @@ document.addEventListener("DOMContentLoaded", function() {
     } 
 
     if (query && query !== langParam) {
-        window.location.href = url + "?lang=" + query;
+        redirect(url + "?lang=" + query);
     }
 });
+
+const languageSelector = document.getElementById('language-selector');
+const translations = {
+    en: {
+        url: "./?select=true"
+    },
+    jp: {
+        url: "./?lang=ja&select=true"
+    },
+    ru: {
+        url: "./?lang=ru&select=true"
+    },
+    zh: {
+        url: "./?lang=zh&select=true"
+    },
+    es: {
+        url: "./?lang=es&select=true"
+    },
+    pt: {
+        url: "./?lang=pt&select=true"
+    },
+    de: {
+        url: "./?lang=de&select=true"
+    },
+    ko: {
+        url: "./?lang=ko&select=true"
+    },
+    fr: {
+        url: "./?lang=fr&select=true"
+    },
+    it: {
+        url: "./?lang=it&select=true"
+    },
+    tr: {
+        url: "./?lang=tr&select=true"
+    }
+};
+
+// Event listener for language change
+languageSelector.addEventListener('change', function() {
+    const selectedLanguage = languageSelector.value;
+    redirect(translations[selectedLanguage].url);
+});
+
+function redirect(url){
+    let select = currentUrl.searchParams.get("select");
+
+    if (!select) {
+        window.location.href = url;
+    }
+}
+
