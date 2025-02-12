136a2e782dc871bc88022970361b8f55a7449a20
MasashiShimamoto <masashishimamoto@gmail.com>
2025-02-12 19:47:47 +0900

    a

---
+++ tradingtools/Trend-Checker-Linear-Regression/js/event.js
@@ -0,0 +1,42 @@
+document.getElementById("mt5-link-top").addEventListener("click", function() {
+    gtag('event', 'cta_click', {
+        'button_name': 'mt5-top',
+        'location': 'first view'
+    });
+});
+document.getElementById("mt4-link-top").addEventListener("click", function() {
+    gtag('event', 'cta_click', {
+        'button_name': 'mt4-top',
+        'location': 'first view'
+    });
+});
+document.getElementById("mt5-link-bottom").addEventListener("click", function() {
+    gtag('event', 'cta_click', {
+        'button_name': 'mt5-bottom',
+        'location': 'CTA view'
+    });
+});
+document.getElementById("mt4-link-bottom").addEventListener("click", function() {
+    gtag('event', 'cta_click', {
+        'button_name': 'mt4-bottom',
+        'location': 'CTA view'
+    });
+});
+document.getElementById("mt5-link-side").addEventListener("click", function() {
+    gtag('event', 'cta_click', {
+        'button_name': 'mt5-side',
+        'location': 'side'
+    });
+});
+document.getElementById("mt4-link-side").addEventListener("click", function() {
+    gtag('event', 'cta_click', {
+        'button_name': 'mt4-side',
+        'location': 'side'
+    });
+});
+document.getElementById("Privacy-Policy").addEventListener("click", function() {
+    gtag('event', 'Privacy-Policy_click', {
+        'button_name': 'Privacy-Policy',
+        'location': 'CTA view'
+    });
+});
