b17adced56521c3cd9908fc7c68776c4a27e32df
MasashiShimamoto <masashishimamoto@gmail.com>
2025-02-15 01:10:52 +0900

    a

--- tradingtools/Trend-Checker-Linear-Regression/js/event.js
+++ tradingtools/Trend-Checker-Linear-Regression/js/event.js
@@ -1,26 +1,29 @@
 // ボタンクリックイベントの共通関数
-function setupButtonEvent(buttonId, buttonName, location) {
+function setupButtonEvent(buttonId, eventName) {
     document.getElementById(buttonId).addEventListener("click", function() {
-        gtag('event', 'cta_click', {
-            'button_name': buttonName,
-            'location': location
+        gtag('event', eventName, {
         });
     });
 }
 
 // 各ボタンのイベント設定
-setupButtonEvent("mt5-link-top", "mt5-top", "first view");
-setupButtonEvent("mt4-link-top", "mt4-top", "first view");
-setupButtonEvent("mt5-link-bottom", "mt5-bottom", "CTA view");
-setupButtonEvent("mt4-link-bottom", "mt4-bottom", "CTA view");
-setupButtonEvent("mt5-link-side", "mt5-side", "side");
-setupButtonEvent("mt4-link-side", "mt4-side", "side");
+setupButtonEvent("mt5-link-top",  "mt5-top");
+setupButtonEvent("mt4-link-top",  "mt4-top");
+setupButtonEvent("mt5-link-bottom","mt5-bottom");
+setupButtonEvent("mt4-link-bottom", "mt4-bottom");
+setupButtonEvent("mt5-link-side",  "mt5-side");
+setupButtonEvent("mt4-link-side",  "mt4-side");
+
 
 // トラッキングする要素のIDリスト
-const elementsToTrack = ["2nd-View-End", "thirdv-title","thirdv-item-1-End",
-    "3rd-View-Video-End","thirdv-title2","problem-End","solve-text","encouragement-End",
-    "thirdv-title3","confident-img-End","thirdv-item-3-End","cta-title","cta-buttons-top",
-    "mt4-link-bottom","last-view-text","bottom"];
+const elementsToTrack = [
+    "2nd-View-End", "thirdv-title", "thirdv-item-1-End",
+    "3rd-View-Video-End", "thirdv-title2", "problem-End", 
+    "solve-text", "encouragement-End", "thirdv-title3", 
+    "confident-img-End", "thirdv-item-3-End", "cta-title", 
+    "cta-buttons-top", "mt4-link-bottom", "last-view-text", 
+    "bottom"
+];
 
 // スクロール時に要素の位置をチェックする関数
 function trackElementView() {
@@ -34,9 +37,14 @@ function trackElementView() {
         const elementTop = elementRect.top;
         const elementBottom = elementRect.bottom;
 
+        // 要素が表示された場合にスクロール進捗をトラッキング
         if (elementTop < windowHeight && elementBottom > 0) {
             element.isTracked = true; // 重複送信を防ぐ
-            gtag('event', 'element_view', {
+
+            // イベント名を要素IDを元に動的に設定
+            const eventName = `scroll_to_${id}`;
+
+            gtag('event', eventName, {
                 'element_id': id,
                 'event_category': 'scroll',
                 'event_label': location.pathname
