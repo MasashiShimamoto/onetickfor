1416f0319bc77b2ebf7862e24fc55674891c926d
MasashiShimamoto <masashishimamoto@gmail.com>
2025-02-18 18:34:37 +0900

    a

--- tradingtools/Trend-Checker-Linear-Regression/js/event.js
+++ tradingtools/Trend-Checker-Linear-Regression/js/event.js
@@ -18,10 +18,11 @@ document.addEventListener("DOMContentLoaded", function() {
   
     // トラッキングする要素のIDリスト
     const elementsToTrack = [
-      "2nd-View-End", "thirdv-title", "thirdv-item-1-End", "thirdv-title2",
-      "problem-End", "thirdv-item-2-End", "encouragement-End", "thirdv-title3",
-      "thirdv-item-img-End", "thirdv-item-3-End", "cta-title", "mt4-link-bottom",
-      "last-view-text", "bottom"
+      "thirdv-item-1-End", 
+      "thirdv-item-2-End", 
+      "thirdv-item-3-End",
+      "CTA-section-End", 
+      "bottom"
     ];
   
     // IntersectionObserverを使用してスクロールトラッキング
